6aba1a32ed622a39bec37cfdc0c93de9ae4b1560
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2017-07-05 10:23:09 +0300

    Create custom user auth model under /app.
    Otherwise the table in the queries will be social_core..customusersocialauth and a migration will be made for that app.

--- app/models.py
+++ app/models.py
@@ -6,7 +6,9 @@ from social_django.models import AbstractUserSocialAuth, DjangoStorage, USER_MOD
 class CustomUserSocialAuth(AbstractUserSocialAuth):
     user = models.ForeignKey(USER_MODEL, related_name='custom_social_auth',
                              on_delete=models.CASCADE)
-
+    class Meta:
+        """Meta data"""
+        app_label = "app"
 
 class CustomDjangoStorage(DjangoStorage):
     user = CustomUserSocialAuth
